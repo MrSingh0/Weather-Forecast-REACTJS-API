a7d58c0dce20b2c9668481d32aab40929e029501
Priyam Kumar <136845755+MrSingh0@users.noreply.github.com>
2024-06-18 18:32:15 +0530

    search.jsx

--- src/search.jsx
+++ src/search.jsx
@@ -12,6 +12,7 @@ export default function SearchBox({ updateInfo }) {
 
     const url = "http://api.openweathermap.org/data/2.5/weather"
     const key = process.env.api_key;
+    console.log(key);
     // a28a24054f662ef99e8052e61e8eb048
 
     let getweather = async () => {
